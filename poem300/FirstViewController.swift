4f57b7ab6609f59e72d0de8ab5687b7625331a00
hatoto <hatoto@icloud.com>
2016-10-07 13:05:00 +0800

    Initial Commit

---
+++ poem300/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  poem300
+//
+//  Created by hatoto on 2016/10/7.
+//  Copyright © 2016年 hatotoINC. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
